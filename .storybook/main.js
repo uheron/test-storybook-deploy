eb21ee1ba6fd8201e7e1ed17cb36fed00c21c1c8
Uliana <uliana.heron@froedtert.com>
2022-12-29 14:56:36 -0600

    test

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,12 @@
 module.exports = {
   addons: [/*existing addons,*/ '@storybook/addon-react-native-web'],
 };
+
+module.exports = {
+  "stories": ["../src/**/*.stories.@(js|jsx|ts|tsx)", "storybook/*.stories.@(js|jsx|ts|tsx"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
+  "framework": "@storybook/web-components",
+  core: {
+    builder: "webpack5"
+  }
+};
